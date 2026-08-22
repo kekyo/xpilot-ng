c5d9ee51694e89bb4a71a9663a90dea8c9d148e9
Kouji Matsui <k@kekyo.net>
2026-08-22 20:55:03 +0900

    refactor: centralize secure random generation

--- src/common/session_token.c
+++ src/common/session_token.c
@@ -9,64 +9,17 @@
  * (at your option) any later version.
  */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#ifdef _WINDOWS
-#ifndef _CRT_RAND_S
-#define _CRT_RAND_S
-#endif
-#endif
-
 #include "xpcommon.h"
 
+#include "secure_random.h"
 #include "session_token.h"
-
-#ifdef _WINDOWS
-static bool Fill_secure_random(void *destination, size_t size)
-{
-    unsigned char *bytes = destination;
-    size_t offset = 0;
-
-    while (offset < size) {
-        unsigned int random_value;
-        size_t remaining;
-        size_t copied;
-
-        if (rand_s(&random_value) != 0)
-            return false;
-        remaining = size - offset;
-        copied = MIN(remaining, sizeof(random_value));
-        memcpy(bytes + offset, &random_value, copied);
-        offset += copied;
-    }
-    return true;
-}
-#else
-static bool Fill_secure_random(void *destination, size_t size)
-{
-    FILE *random_file;
-    size_t bytes_read;
-
-    random_file = fopen("/dev/urandom", "rb");
-    if (random_file == NULL)
-        return false;
-    bytes_read = fread(destination, 1, size, random_file);
-    if (fclose(random_file) != 0 || bytes_read != size)
-        return false;
-    return true;
-}
-#endif
 
 bool Session_token_generate(session_token_t *token)
 {
     if (token == NULL)
         return false;
-    if (!Fill_secure_random(token, sizeof(*token))) {
-        memset(token, 0, sizeof(*token));
+    if (!Secure_random_fill(token, sizeof(*token)))
         return false;
-    }
     return true;
 }
 
--- tests/test_session_token.c
+++ tests/test_session_token.c
@@ -1,12 +1,17 @@
 #include "test_helpers.h"
 
+#include "secure_random.h"
 #include "session_token.h"
 
 int main(void)
 {
+    unsigned char random_bytes[16] = { 0 };
     session_token_t generated;
     session_token_t copy;
 
+    TEST_CHECK(Secure_random_fill(random_bytes, sizeof(random_bytes)));
+    TEST_CHECK(!Secure_random_fill(NULL, sizeof(random_bytes)));
+    TEST_CHECK(!Secure_random_fill(random_bytes, 0));
     TEST_CHECK(Session_token_generate(&generated));
     copy = generated;
     TEST_CHECK(Session_token_equal(&generated, &copy));
